f0e87b67815c67720fdcd04e9e71703eca536724
sven.lehmann <sven.lehmann@student.hpi.uni-potsdam.de>
2014-10-30 09:28:52 +0100

    added php5-curl to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,12 @@ mkdir $JBOSS_HOME
 mkdir log
 export LOG_FILE=`pwd`/log/log.txt
 touch $LOG_FILE
-echo $LOG_FILE
-# installing software
+
+echo "installing software"
 sudo apt-get -y install apache2 >> $LOG_FILE
 sudo apt-get -y install libapache2-mod-php5 >> $LOG_FILE
-sudo apt-get -y install git >> $LOG_FILE
+sudo apt-get -y install php5-curl >> $LOG_FILE
+sudo /etc/init.d/apache2 restart >> $LOG_FILE
 sudo apt-get -y install ant >> $LOG_FILE
 sudo apt-get -y install curl >> $LOG_FILE
 sudo apt-get -y install openjdk-7-jdk >> $LOG_FILE
@@ -25,22 +26,22 @@ sudo apt-get update >> $LOG_FILE
 sudo apt-get -y install jenkins >> $LOG_FILE
 sudo apt-get -y install unzip >> $LOG_FILE
 
-# setting up webserver
+echo "setting up webserver"
 curl -s -o ~/i2b2webclient-1703a.zip https://dl.dropboxusercontent.com/u/23263423/i2b2webclient-1703a.zip >> $LOG_FILE
 unzip i2b2webclient-1703a.zip >> $LOG_FILE
 sudo cp -r ~/webclient /var/www/html/ >> $LOG_FILE
 sudo cp -r $I2B2_HOME/admin /var/www/html/ >> $LOG_FILE
 
-# Download jboss
+echo "downloading jboss"
 curl -s -o ~/jboss-as-7.1.1.Final.zip https://dl.dropboxusercontent.com/u/23263423/jboss-as-7.1.1.Final.zip >> $LOG_FILE
 unzip -d $JBOSS_HOME jboss-as-7.1.1.Final.zip >> $LOG_FILE
 
-# configuring cells
+echo "configuring cells"
 cd $I2B2_HOME >> $LOG_FILE
 sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */build.properties -i >> $LOG_FILE
 sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */etc/spring/*_application_directory.properties -i >> $LOG_FILE
 
-# build cells
+echo "building cells"
 cd $I2B2_HOME >> $LOG_FILE
 cd edu.harvard.i2b2.server-common >> $LOG_FILE
 sudo ant clean dist deploy jboss_pre_deployment_setup >> $LOG_FILE
@@ -51,9 +52,9 @@ sudo ant -f master_build.xml clean build-all deploy >> $LOG_FILE
 cd ../edu.harvard.i2b2.crc/ >> $LOG_FILE
 sudo ant -f master_build.xml clean build-all deploy >> $LOG_FILE
 
-# start jboss
+echo "starting jboss"
 #sudo screen -dmS "I2B2" sudo sh `echo $JBOSS_HOME`/bin/standalone.sh -b 0.0.0.0
-sudo sh `echo $JBOSS_HOME`/bin/standalone.sh -b 0.0.0.0 >> $LOG_FILE
+sudo sh `echo $JBOSS_HOME`/bin/standalone.sh -b 0.0.0.0
 
 #clear
 #echo "Success! Setup finished!";

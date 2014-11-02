b2d5d51edd13f8a71710a7c246e8c2840c54445e
Philipp Schirmer <philipp@schirmer-online.de>
2014-11-02 17:14:13 +0100

    Revert last change

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-echo "configuring cells"
-sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */build.properties -i
-sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */etc/spring/*_application_directory.properties -i
-
-echo "building cells"
 cd edu.harvard.i2b2.server-common
 sudo ant clean dist deploy jboss_pre_deployment_setup
 cd ../edu.harvard.i2b2.pm

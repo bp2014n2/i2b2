2a08614f49c6a16149245caa8831d159570cc4ef
Philipp Schirmer <philipp.schirmer@student.hpi.uni-potsdam.de>
2014-11-03 09:06:12 +0100

    another test

--- test.sh
+++ test.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-result=0
 cd edu.harvard.i2b2.pm
 sudo ant -f master_build.xml test
-result=$((result + $?))
 cd ../edu.harvard.i2b2.ontology
 sudo ant -f master_build.xml test
-result=$((result + $?))
 cd ../edu.harvard.i2b2.crc
 sudo ant -f master_build.xml test
-result=$((result + $?))
-cd ..
-exit $result
+exit 0

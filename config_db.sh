95287558fe48cd7a0f67e55c0cbddf854a2ec71c
Philipp Schirmer <philipp.schirmer@student.hpi.uni-potsdam.de>
2015-01-09 10:17:35 +0100

    included env for ant scripts, config_db.sh has an optional parameter

--- config_db.sh
+++ config_db.sh
@@ -1,2 +1,11 @@
-sed "s|54\.93\.194\.65:5432|`echo localhost:5432`|g" */etc/jboss/*-ds.xml -i
-sed "s|54\.93\.194\.65:5432|`echo localhost:5432`|g" */etc/spring/*LoaderApplicationContext.xml -i
+#!/bin/bash
+
+$db_loc = "localhost:5432"
+
+if [ $# -ge 1 ]
+then
+    $db_loc = $1
+fi
+
+sed "s|54\.93\.194\.65:5432|`echo $db_loc`|g" */etc/jboss/*-ds.xml -i
+sed "s|54\.93\.194\.65:5432|`echo $db_loc`|g" */etc/spring/*LoaderApplicationContext.xml -i

759384d53998564c74e2f2ec37f6c4b807404ad6
Philipp Schirmer <philipp@schirmer-online.de>
2015-01-20 20:27:11 +0100

    removed db related stuff

--- insert_basecodes.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE i2b2metadata.schemes;
-INSERT INTO i2b2metadata.schemes (c_key, c_name, c_description) VALUES ('ATC:', 'ATC', 'ATC Code');
-INSERT INTO i2b2metadata.schemes (c_key, c_name, c_description) VALUES ('ICD:', 'ICD', 'ICD Code');
-INSERT INTO i2b2metadata.schemes (c_key, c_name, c_description) VALUES ('FG:', 'Fachgruppen', 'Fachgruppen');
-INSERT INTO i2b2metadata.schemes (c_key, c_name, c_description) VALUES ('STAMM:ALTER:', 'Alter', 'Alter');
--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,5 @@ sudo rm -rf /var/www/html/admin
 sudo rm -rf /var/www/html/webclient
 
 sudo rm -rf ~/jboss
-sudo rm -rf ~/eha-i2b2
+sudo rm -rf ~/i2b2
 sudo rm -rf ~/log

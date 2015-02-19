af9bc41271bcd055ccd73310f098f451d96e6395
Philipp Schirmer <philipp@schirmer-online.de>
2015-02-19 19:39:46 +0100

    Further work on risk prediction

--- reportScripts/Logistic Regression/mainscript.r
+++ reportScripts/Logistic Regression/mainscript.r
@@ -1,90 +1,8 @@
-executeWrappedQuery <- function(con, raw_query) {
-  
-  query <- strwrap(raw_query, width=10000, simplify=TRUE)
-  rs <- dbSendQuery(con, query)
-  
-  return(fetch(rs,n=-1))
-  
-}
-require(RPostgreSQL)
+setwd('/home/ubuntu/i2b2/reportScripts/Logistic Regression/')
+source("utils.r")
+source("i2b2.r")
+
 require(Matrix)
-
-drv <- dbDriver("PostgreSQL")
-con <- dbConnect(drv, dbname="i2b2", host="localhost", user="i2b2demodata", password="demouser", port="5432")
-
-queries.observations <- "WITH ICD AS (
-SELECT *
-FROM i2b2demodata.observation_fact
-WHERE concept_cd LIKE '%s%%'
-AND (start_date >= '%i-01-01T00:00:00' AND start_date <= '%i-01-01T00:00:00') 
-)
-SELECT patient_num, concept_cd, COUNT(*) AS count
-FROM (
-(SELECT patient_num, substring(concept_cd from 0 for position('.' in concept_cd)) AS concept_cd
-FROM ICD
-WHERE POSITION('.' in concept_cd) <> 0)
-UNION ALL
-(SELECT patient_num, concept_cd AS concept_cd
-FROM ICD
-WHERE POSITION('.' in concept_cd) = 0)
-) a
-GROUP BY patient_num, concept_cd"
-
-queries.patients <- "SELECT DISTINCT patient_num
-FROM i2b2demodata.patient_dimension"
-
-queries.features <- "WITH ICD AS (
-SELECT *
-FROM i2b2demodata.concept_dimension
-WHERE concept_cd LIKE '%s%%'
-)
-SELECT DISTINCT concept_cd
-FROM (
-(SELECT substring(concept_cd from 0 for position('.' in concept_cd)) AS concept_cd
-FROM ICD
-WHERE POSITION('.' in concept_cd) <> 0)
-UNION ALL
-(SELECT concept_cd AS concept_cd
-FROM ICD
-WHERE POSITION('.' in concept_cd) = 0)
-) a"
-
-generateModel <- function(observations, features, patients) {
-  model <- Matrix(rep(0,times=length(features)*length(patients)), ncol=length(features), nrow=length(patients), sparse=TRUE)
-  
-  rownames(model) <- patients
-  colnames(model) <- features
-  
-  transformToModel <- function(row, patients, features, model) {
-    model[row['patient_num'], row['concept_cd']] <<- strtoi(row['count'])
-  }
-  
-  apply(observations, 1, transformToModel, patients=patients, features=features, model=model)
-  
-  return(model)
-}
-
-model_year <- strtoi(report.input['Model year'])
-prediction_year <- strtoi(report.input['Prediction year'])
-model_target_interval <- strtoi(report.input['Model target interval'])
-target_year <- model_year + model_target_interval
-target_icd <- report.input['Target concept']
-feature_filter <- 'ICD9:'
-report.output[['Information']] <- sprintf('Model: %i, Target: %i, New: %i', model_year, target_year, prediction_year)
-
-observations <- executeWrappedQuery(con, sprintf(queries.observations, feature_filter, model_year, model_year + 1))
-new_observations <- executeWrappedQuery(con, sprintf(queries.observations, feature_filter, prediction_year, prediction_year + 1))
-target_observations <- executeWrappedQuery(con, sprintf(queries.observations, target_icd, target_year, target_year + 1))
-features <- executeWrappedQuery(con, sprintf(queries.features, feature_filter))$concept_cd
-patients <- executeWrappedQuery(con, queries.patients)$patient_num
-
-model <- generateModel(observations, features, patients)
-new_model <- generateModel(new_observations, features, patients)
-target_model <- generateModel(target_observations, c(target_icd), patients)
-
-dbDisconnect(con)
-
-target <- sign(target_model[,1])
 
 predictRisk <- function(model, target, newdata) {
   
@@ -105,6 +23,86 @@ predictRisk <- function(model, target, newdata) {
   
 }
 
+generateFeatureMatrix <- function(observations, features, patients) {
+  model <- Matrix(rep(0,times=length(features)*length(patients)), ncol=length(features), nrow=length(patients), sparse=TRUE)
+  
+  rownames(model) <- patients
+  colnames(model) <- features
+  
+  transformToModel <- function(row, patients, features, model) {
+    model[row['patient_num'], row['concept_cd']] <<- strtoi(row['count'])
+  }
+  
+  apply(observations, 1, transformToModel, patients=patients, features=features, model=model)
+  
+  return(model)
+}
+
+queries.observations <- "WITH ICD AS (
+  SELECT *
+  FROM i2b2demodata.observation_fact
+  WHERE concept_cd LIKE '%s%%'
+  AND (start_date >= '%i-01-01T00:00:00' AND start_date <= '%i-01-01T00:00:00') 
+)
+SELECT patient_num, concept_cd, COUNT(*) AS count
+FROM (
+    (SELECT patient_num, substring(concept_cd from 0 for position('.' in concept_cd)) AS concept_cd
+    FROM ICD
+    WHERE POSITION('.' in concept_cd) <> 0)
+  UNION ALL
+    (SELECT patient_num, concept_cd AS concept_cd
+    FROM ICD
+    WHERE POSITION('.' in concept_cd) = 0)
+) a
+GROUP BY patient_num, concept_cd"
+
+queries.patients <- "SELECT DISTINCT patient_num
+FROM i2b2demodata.patient_dimension"
+
+queries.features <- "WITH ICD AS (
+  SELECT *
+  FROM i2b2demodata.concept_dimension
+  WHERE concept_cd LIKE '%s%%'
+)
+SELECT DISTINCT concept_cd
+FROM (
+    (SELECT substring(concept_cd from 0 for position('.' in concept_cd)) AS concept_cd
+    FROM ICD
+    WHERE POSITION('.' in concept_cd) <> 0)
+  UNION ALL
+    (SELECT concept_cd AS concept_cd
+    FROM ICD
+    WHERE POSITION('.' in concept_cd) = 0)
+) a"
+
+queries.concept_cd <- "SELECT concept_cd
+FROM i2b2demodata.concept_dimension
+WHERE concept_path LIKE '%s'"
+
+model_year <- strtoi(report.input['Model year'])
+prediction_year <- strtoi(report.input['Prediction year'])
+model_target_interval <- strtoi(report.input['Model target interval'])
+target_concept <- report.input['Target concept']
+feature_filter <- 'ICD9:'
+target_year <- model_year + model_target_interval
+
+con <- initializeCRCConnection()
+
+observations <- executeQuery(con, strunwrap(queries.observations), feature_filter, model_year, model_year + 1)
+new_observations <- executeQuery(con, strunwrap(queries.observations), feature_filter, prediction_year, prediction_year + 1)
+target_icd <- executeQuery(con, strunwrap(queries.concept_cd), gsub("[\\]", "\\\\\\\\", target_concept))$concept_cd
+target_observations <- executeQuery(con, strunwrap(queries.observations), target_icd, target_year, target_year + 1)
+features <- executeQuery(con, strunwrap(queries.features), feature_filter)$concept_cd
+patients <- executeQuery(con, strunwrap(queries.patients))$patient_num
+
+destroyCRCConnection(con)
+
+model <- generateFeatureMatrix(observations, features, patients)
+new_model <- generateFeatureMatrix(new_observations, features, patients)
+target_model <- generateFeatureMatrix(target_observations, c(target_icd), patients)
+
+target <- sign(target_model[,1])
+
 # print result
 
 prediction <- predictRisk(model, target, new_model)
@@ -113,4 +111,5 @@ colnames(sorted_prediction) <- c('patient_num', 'probability')
 sorted_prediction <- sorted_prediction[order(-sorted_prediction$probability),]
 rownames(sorted_prediction) <- NULL
 
+report.output[['Information']] <- sprintf('Model: %i, Target: %i, New: %i', model_year, target_year, prediction_year)
 report.output[['Prediction']] <- sorted_prediction

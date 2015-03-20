8fc3c85e524b8577e76d0e008d365dd453fa3e95
Philipp Schirmer <philipp@schirmer-online.de>
2015-03-20 10:43:32 +0100

    jar path wd safe

--- GIRIXScripts/lib/i2b2.ont.config.r
+++ GIRIXScripts/lib/i2b2.ont.config.r
@@ -4,5 +4,5 @@ i2b2$ont$db['username'] <- 'i2b2metadata'
 i2b2$ont$db['password'] <- 'demouser'
 i2b2$ont$db['name'] <- 'i2b2'
 i2b2$ont$db['class'] <- 'org.postgresql.Driver'
-i2b2$ont$db['jar'] <- 'lib/postgresql-9.2-1002.jdbc4.jar'
+i2b2$ont$db['jar'] <- paste(getwd(), 'lib/postgresql-9.2-1002.jdbc4.jar', sep="/")
 i2b2$ont$db['type'] <- 'postgresql'

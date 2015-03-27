1ec90dfe03b61bbd3d3179980582d5f9905aceaf
Carl Ambroselli <git@carl-ambroselli.de>
2015-03-27 10:41:56 +0100

    Refactoring, recognizing scriptlet dir automatically

--- GIRIXScripts/ReportGenerator/ReportGen/main.r
+++ GIRIXScripts/ReportGenerator/ReportGen/main.r
@@ -1,3 +1,4 @@
+# Install packages if necessary
 list.of.packages <- c("knitr", "extrafont")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -7,12 +8,15 @@ library(extrafont)
 
 source("lib/i2b2.r")
 
+scriptlet.fulldir <- dirname(sys.frame(1)$ofile)
+scriptlet.dir <- sub(".*/(.*)/ReportGen/.*", "\\1", scriptlet.fulldir,perl=TRUE)
+
 options(warn=-1)
 
 # Unique information
 randomNumber <- floor(runif(1, 10000000, 99999999))
 currentTimestamp <- print(as.numeric(Sys.time()))
-tmpFolder <- paste('ReportGenerator/tmp/', randomNumber, currentTimestamp, "/", sep='')
+tmpFolder <- paste(scriptlet.dir, '/tmp/', randomNumber, currentTimestamp, "/", sep='')
 
 dir.create(tmpFolder, mode="0777")
 dir.create(paste(tmpFolder, '/plots',  sep=''), mode="0777")
@@ -22,7 +26,6 @@ input <- girix.input
 #Setup Knitr
 
 # Deactivates code output globally
-library(knitr)
 knitr::opts_chunk$set(echo=FALSE, fig.path=paste0(tmpFolder, 'plots/'), cache=FALSE, dev='svg', results='hide')
 opts_knit$set(progress = FALSE, verbose = FALSE)
 opts_chunk$set(fig.width=5, fig.height=5)
@@ -39,11 +42,11 @@ knit_hooks$set(plot = function(x, options) {
 # Generate File
 fileName <- 'main.html'
 if(input["requestDiagram"] == "all"){
-  knit('ReportGenerator/layout/main.Rhtml', output=paste(tmpFolder, fileName, sep=""))
+  knit(paste0(scriptlet.dir, '/layout/main.Rhtml'), output=paste(tmpFolder, fileName, sep=""))
 } else if(input["requestDiagram"] == "age_histogram") {
-  knit('ReportGenerator/diagrams/age_histogram/layout.Rhtml', output=paste(tmpFolder, fileName, sep=""))
+  knit(paste0(scriptlet.dir, '/diagrams/age_histogram/layout.Rhtml'), output=paste(tmpFolder, fileName, sep=""))
 } else if(input["requestDiagram"] == "frequence_of_visits_chart") {
-  knit('ReportGenerator/diagrams/frequence_of_visits_chart/layout.Rhtml', output=paste(tmpFolder, fileName, sep=""))
+  knit(paste0(scriptlet.dir, '/diagrams/frequence_of_visits_chart/layout.Rhtml'), output=paste(tmpFolder, fileName, sep=""))
 }
 
 # Output

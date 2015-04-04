bd36b604bdca74ef3d1d8ecbc00c1c3cc7dfd4c4
Philipp Schirmer <philipp@schirmer-online.de>
2015-04-04 18:49:06 +0200

    Revert smoothing for performance issues #118

--- GIRIXScripts/Risk Prediction/mainscript.r
+++ GIRIXScripts/Risk Prediction/mainscript.r
@@ -171,14 +171,14 @@ if(smooth_lines) {
 }
 plot(performance$precrec, main='Precision/Recall curve', lty="dotted")
 if(smooth_lines) {
-  
-  y <- performance$precrec@y.values[[1]]
-  
-  for(i in rev(seq_along(y))) {
-    y[i] <- max(y[i:length(y)])
-  }
-  performance$precrec@y.values[[1]] <- y
-  lines(performance$precrec@x.values[[1]], performance$precrec@y.values[[1]], col="red", lwd=2)
+  smoothedLine(performance$precrec@x.values[[1]], performance$precrec@y.values[[1]])
+  #y <- performance$precrec@y.values[[1]]
+  #
+  #for(i in rev(seq_along(y))) {
+  #  y[i] <- max(y[i:length(y)])
+  #}
+  #performance$precrec@y.values[[1]] <- y
+  #lines(performance$precrec@x.values[[1]], performance$precrec@y.values[[1]], col="red", lwd=2)
 }
 
 rm(girix.input, girix.concept.names, girix.events, girix.modifiers, girix.observations, girix.observers, girix.patients); gc()

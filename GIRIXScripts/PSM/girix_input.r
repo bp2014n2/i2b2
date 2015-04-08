926ead7a1cface1d4955ebcf164dd1bff6d4ebc4
peterHeuz <jan-peter.heuzeroth@student.hpi.de>
2015-04-08 15:49:49 +0200

    PSM working with proper interval (evaluated treatment) and integrated in GIRIX

--- GIRIXScripts/PSM/girix_input.r
+++ GIRIXScripts/PSM/girix_input.r
@@ -9,5 +9,6 @@ girix.modifiers <- c()
 girix.events <- c()
 girix.observers <- c()
 
-girix.input['Treatment group'] <- 3   #in webclient: L10 Pemphiguskr
-girix.input['Control group'] <- 12
+girix.input['Treatment group'] <- "3"   #in webclient: L10 Pemphiguskr, 7. april, 487 patients
+girix.input['Control group'] <- "12"    #in webclient: "G70-G73 Krankhe@10:47:54" , 7. april, 8067 patients
+girix.input['Treatment date'] <- "01/01/2009"
--- GIRIXScripts/PSM/mainscript.r
+++ GIRIXScripts/PSM/mainscript.r
@@ -1,6 +1,6 @@
 require(Matrix)
 require(Matching)
-
+q
 source("../lib/i2b2.r", chdir=TRUE)
 source("../lib/dataPrep.r", chdir=TRUE)
 source("logic.r")
@@ -9,12 +9,16 @@ if(!exists('girix.input')) {
   source("girix_input.r")
 }
 
-patientset.t.id <- girix.input['Treatment group']
-patientset.c.id <- girix.input['Control group']
+# girix input processing
+patientset.t.id <- strtoi(girix.input['Treatment group'])
+patientset.c.id <- strtoi(girix.input['Control group'])
+treatmentDate <- i2b2DateToPOSIXlt(girix.input['Treatment date'])
+
+print(girix.input['Treatment date'])
 
 # i2b2 date format (MM/DD/YYYY)
 #interval <- list(start='01/01/2000', end='12/31/2015') 
-interval <- list(start=i2b2DateToPOSIXlt('01/01/2000'), end=i2b2DateToPOSIXlt('12/31/2015'))
+interval <- list(start=i2b2DateToPOSIXlt('01/01/2000'), end=treatmentDate)
 
 print("getting featureMatrices")
 featureMatrix.t <- DataPrep.generateFeatureMatrixFromPatientSet(patient_set=patientset.t.id, interval=interval)
@@ -24,13 +28,14 @@ featureMatrix <- rbind2(featureMatrix.t, featureMatrix.c)
 
 print("calculating probabilities")
 target.vector <- c(rep(1, each=nrow(featureMatrix.t)),rep(0, each=nrow(featureMatrix.c)))
-probabilities <- ProbabilitiesOfLogRegFittingWithTargetVector(featureMatrix=featureMatrix, target.vector)
+probabilities <- ProbabilitiesOfLogRegFittingWithTargetVector(featureMatrix=featureMatrix, target.vector=target.vector)
 
 print("matching")
 matched <- Match(Tr=probabilities[,2], X=probabilities[,1], M=1, exact=TRUE, ties=TRUE, version="fast")
 
 print("outputting")
-output <- cbind(rownames(to.match[matched$index.treated,]), to.match[matched$index.treated,"Probability"], rownames(to.match[matched$index.control,]), to.match[matched$index.control, "Probability"])
+output <- cbind(rownames(featureMatrix[matched$index.treated,]), probabilities[matched$index.treated,1], 
+				rownames(featureMatrix[matched$index.control,]), probabilities[matched$index.control,1])
 colnames(output) <- c("Treatment group patient number", "Score", "Control group patient number", "Score")
 rownames(output) <- c()
 

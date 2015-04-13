2afe5cb096e17941dcd14c8d4ea2fdf73b418e9a
peterHeuz <jan-peter.heuzeroth@student.hpi.de>
2015-04-13 18:31:50 +0200

    cost function for single year added

--- GIRIXScripts/PSM/logic.r
+++ GIRIXScripts/PSM/logic.r
@@ -52,7 +52,10 @@ ProbabilitiesOfLogRegFittingWithTargetVector <- function(featureMatrix, target.v
   return(patient.probabilities)  
 }
 
-GetYearCosts <- function(patient_nums) {
+GetAllYearCosts <- function(patient_nums) {
+  # returns summe_aller_kosten for each patient for every year
+  # to do: remove limit
+  # to do: integrate to lib dataPrep.r
   num.string <- toString(patient_nums[1])
   for (i in patient_nums) {
     num.string <- paste0(num.string, ", ", i)
@@ -65,5 +68,25 @@ GetYearCosts <- function(patient_nums) {
     ORDER BY patient_num, datum_year
     LIMIT 20;"
 
-  return(executeCRCQuery(query, num.string))  
+  result <- executeCRCQuery(query, num.string)
+  return(result)
+}
+
+GetOneYearCosts <- function(patient_nums, year) {
+  # returns summe_aller_kosten for each patient for one specific year
+  # to do: integrate to lib dataPrep.r
+  num.string <- toString(patient_nums[1])
+  for (i in patient_nums) {
+    num.string <- paste0(num.string, ", ", i)
+  }
+
+  query <- "SELECT patient_num, SUM(summe_aller_kosten)
+    FROM i2b2demodata.AVK_FDB_T_Leistungskosten 
+    WHERE patient_num IN (%s)
+    AND CAST(EXTRACT(YEAR FROM datum) AS INT) = %d
+    GROUP BY patient_num
+    ORDER BY patient_num"
+
+  result <- executeCRCQuery(query, num.string, year)
+  return(result)
 }

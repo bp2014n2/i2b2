69ca6a207bf0fdec8b429a52de7f30aac8fee94a
peterHeuz <jan-peter.heuzeroth@student.hpi.de>
2015-04-14 10:30:40 +0200

    output prettified & server integration preparation

--- GIRIXScripts/PSM/mainscript.r
+++ GIRIXScripts/PSM/mainscript.r
@@ -14,10 +14,7 @@ patientset.t.id <- strtoi(girix.input['Treatment group'])
 patientset.c.id <- strtoi(girix.input['Control group'])
 treatmentDate <- i2b2DateToPOSIXlt(girix.input['Treatment date'])
 
-print(girix.input['Treatment date'])
-
 # i2b2 date format (MM/DD/YYYY)
-#interval <- list(start='01/01/2000', end='12/31/2015') 
 interval <- list(start=i2b2DateToPOSIXlt('01/01/2000'), end=treatmentDate)
 
 print("getting featureMatrices")
@@ -37,7 +34,7 @@ print("getting and calculating costs")
 treatmentYear <- as.numeric(format(treatmentDate, "%Y"))
 previousYear <- treatmentYear -1
 pnums.treated <- rownames(featureMatrix)[matched$index.treated]
-pnums.control <- rownames(featureMatrix)[matched$index.control]  # contains together with pnums.treated the matching (order matters)
+pnums.control <- rownames(featureMatrix)[matched$index.control]  # contains together with pnums.treated the matching information(order matters)
 costs.tY <- GetOneYearCosts(c(pnums.treated, pnums.control), treatmentYear)
 costs.pY <- GetOneYearCosts(c(pnums.treated, pnums.control), previousYear)
 
@@ -46,9 +43,11 @@ output <- matrix()
 output <- cbind(pnums.treated, probabilities[matched$index.treated, "probabilities"], costs.pY[pnums.treated,"sum"], costs.tY[pnums.treated,"sum"],
 				pnums.control, probabilities[matched$index.control, "probabilities"], costs.pY[pnums.control,"sum"], costs.tY[pnums.control,"sum"])
 
-colnames(output) <- c("Treatment group patient number", "Score", "Costs at year before treatment", "Cost at treatment year", 
-					  "Control group patient number", "Score", "Costs at year before treatment", "Cost at treatment year")
+colnames(output) <- c("Treatment group p_num", "Score", "Costs year before", "Costs treatment year", 
+					  "Control group p_num", "Score", "Costs year before", "Costs treatment year")
 rownames(output) <- c()
 
 girix.output[["Matched patients"]] <- output
-girix.output[["Matching description"]] <- "No dummy matching anymore ;)"
+girix.output[["Matching description"]] <- "Verbose labels of columns: patient number (treatment group), Propensity Score, 
+										  Overall costs of patient in the year before treatment, Overall costs of patient in the year of treatment.
+										  Simulatenously for the following four columns for patients of control group"

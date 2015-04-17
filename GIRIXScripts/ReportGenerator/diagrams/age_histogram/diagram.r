16d19c931eba8e53ef912c2a7f3917828aae0fa5
Carl Ambroselli <git@carl-ambroselli.de>
2015-04-17 11:21:34 +0200

    Refactored age_histogram

--- GIRIXScripts/ReportGenerator/diagrams/age_histogram/diagram.r
+++ GIRIXScripts/ReportGenerator/diagrams/age_histogram/diagram.r
@@ -17,83 +17,106 @@ hist_age_extended <- function(patients) {
   ages.hist$max <- utils.roundUp(max(ages.hist$counts))
 
   # Setting the layout
-  # omi: Outer border
-  # mai: Inner border
-  # mpg: Abstand Achsenbeschriftung
   par(
-    omi=c(0.65,0.25,0.75,0.75), # Outer border
-    mai=c(0.3,1.5,0.35,0),      # Inner border
+    omi=c(0.37,0,0,0),             # Outer border
+    mai=c(0.3,0.9,0.35,0.3),    # Inner border
     mgp=c(0,0,0),               # Space axle text
     family="Lato",              # Font
-    las=1                       # Style of axle labels (horizontal)
+    las=1,                      # Style of axle labels (horizontal)
+    col=darkGray                # Default color
   )  
 
-  # Creating graphic
+  # Creating the plot
   x<-barplot(
-    ages.hist$counts,
-    names.arg=F,
-    horiz=T,
-    border=NA,
-    xlim=c(0,ages.hist$max),
-    col="grey", 
-    cex.names=0.85,axes=T
+    ages.hist$counts,           # Print the counts
+    horiz=T,                    # Horizontal
+    border=NA,                  # No border
+    xlim=c(0,ages.hist$max),    # Limits to be not always at 100%
+    col=set.alpha(baseColor, 0.6), # Color of bars 
+    axes=F                      # Axes are created with text labels later
+  )
+  
+  # Background bars
+  for (i in 0:4) {
+    rect(
+      ages.hist$max/10*2*i,     # X start
+      0,                        # Y start
+      ages.hist$max/10*(2*i+2), # X end
+      20.7,                     # Y end
+      col=set.alpha(baseColor, ((i)%%2+1) * 0.1), # Alternating color
+      border=NA                 # No borders
+    )
+  }
+
+  # Highlight the highest value
+  overlay<-c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+  indexOfMax <- which.max(ages.hist$counts)
+  overlay[indexOfMax] <- max(ages.hist$counts) # Print only a overlay for the highest 
+  x2<-barplot(
+    overlay,                    # Array of values to overlay
+    horiz=T,                    # Horizontal
+    border=NA,                  # No border
+    xlim=c(0,ages.hist$max),    # Limits of original plot
+    col=baseColor,              # Color
+    axes=F,                     # Don't print axes
+    add=T                       # Additive
   )
 
-  
-  # Blue background bars
-  rect(0,0,ages.hist$max/10*2,20.7,col=rgb(191,239,255,80,maxColorValue=255),border=NA)
-  rect(ages.hist$max/10*2,0,ages.hist$max/10*4,20.7,col=rgb(191,239,255,120,maxColorValue=255),border=NA)
-  rect(ages.hist$max/10*4,0,ages.hist$max/10*6,20.7,col=rgb(191,239,255,80,maxColorValue=255),border=NA)
-  rect(ages.hist$max/10*6,0,ages.hist$max/10*8,20.7,col=rgb(191,239,255,120,maxColorValue=255),border=NA)
-  rect(ages.hist$max/10*8,0,ages.hist$max,20.7,col=rgb(191,239,255,80,maxColorValue=255),border=NA)
-
-  wert2<-c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-  wert2[which.max(ages.hist$counts)] <- max(ages.hist$counts)
-  farbe2<-rgb(255,0,210,maxColorValue=255)
-  x2<-barplot(wert2,names.arg=F,horiz=T,border=NA,xlim=c(0,100),
-                col=farbe2,cex.names=0.85,axes=F,add=T)
-
-  #Grafik beschriften
+  #Y Legend
   for (i in 1:length(ages.hist$counts))
   {
-    if (ages.hist$counts[i] > ages.hist$mean) 
-        {schrift<-"Lato"} else {schrift<-"Lato"}
-    if(i == 1)
-      text(ages.hist$max/100*(-2),x[i],paste(0, "-", ages.hist$breaks[i+1]-1),xpd=T,adj=1,cex=0.85,family=schrift)
-    if(i == length(ages.hist$counts))
-      text(ages.hist$max/100*(-2),x[i],paste(">=", ages.hist$breaks[i]),xpd=T,adj=1,cex=0.85,family=schrift)
-    else
-      text(ages.hist$max/100*(-2),x[i],paste(ages.hist$breaks[i], "-", ages.hist$breaks[i+1]-1),xpd=T,adj=1,cex=0.85,family=schrift)
+    legendText <- paste(ages.hist$breaks[i], "-", ages.hist$breaks[i+1]-1)
+
+    # Oldest groupt gets >= interval
+    if(i == length(ages.hist$counts)) {
+      legendText <- paste(">=", ages.hist$breaks[i])
+    } 
+     
+    # Y legend label 
+    text(
+      (-0.02)*ages.hist$max,    # If the scale is bigger the pixel scale is smaller so relative and 1/50 negative 
+      x[i],                     # Position of the i'th bar
+      legendText,               # Text left of the chart for this row
+      xpd=T,                    # Plotting clipped to figure region
+      adj=1                     # X adjustment ot labels   
+    )
+   
+    # Bar label (hide for bars with less than 10% of maximum)
     if(ages.hist$counts[i] > ages.hist$max/10) {
-      text(ages.hist$counts[i]-(ages.hist$max/100*0.1),x[i],ages.hist$counts[i],xpd=T,adj=1,cex=0.55,family=schrift)
+      text(
+        ages.hist$counts[i],    # X Position
+        x[i],                   # Position of the i'th bar
+        ages.hist$counts[i],    # Text
+        xpd=T,                  # Plotting clipped to figure region
+        adj=1.2,                # Move text to left to be on the bar
+        cex=0.8,                # Font size 80%
+        col="white"
+      )
     }
   }
 
-  arrows(ages.hist$mean,-0.25,ages.hist$mean,20.75,lwd=1.5,length=0,xpd=T,col="skyblue3") 
+  # Average line
+  arrows(ages.hist$mean,-0.25,ages.hist$mean,20.75,lwd=1.5,length=0,xpd=T,col=accentColor[1]) 
   arrows(ages.hist$mean,-0.25,ages.hist$mean,0,lwd=3,length=0,xpd=T)
   arrows(ages.hist$mean,20.75,ages.hist$mean,21,lwd=3,length=0,xpd=T)
   text(ages.hist$mean,22,"Average",adj=0.5,xpd=T,cex=0.65,font=3)
+
+  # Header
   text(ages.hist$mean,21.5,round(ages.hist$mean, digits=0)+20,adj=0.5,xpd=T,cex=0.65,family="Lato",font=4)
   text(ages.hist$max/100*(-5.5),21.3,"Age",adj=0.5,xpd=T,cex=0.65,font=3)
   text(ages.hist$max,21.3,"Values are absolute",adj=1,xpd=T,cex=0.65,font=3)
   
   # X-Legend
-  mtext(c(0,ages.hist$max/10*2,ages.hist$max/10*4,ages.hist$max/10*6,ages.hist$max/10*8,ages.hist$max),at=c(0,ages.hist$max/10*2,ages.hist$max/10*4,ages.hist$max/10*6,ages.hist$max/10*8,ages.hist$max),1,line=0,cex=0.80)
+  legend_positions <- c(0,2,4,6,8,10) * 0.1 * ages.hist$max
+  mtext(legend_positions,at=legend_positions,1,line=0,cex=0.80)
 
-  # Titles
-  mtext("Age Distribution",3,line=1.3,adj=0,cex=1.2,family="Lato",outer=T)
-  
-  if(!is.null(params$limit)) {
-    mtext(paste0("Histogram for first ", params$limit, " patients"),3,line=0,adj=0,cex=0.9,outer=T)
-  } else {
-    mtext(paste0("Histogram for all patients"),3,line=0,adj=0,cex=0.9,outer=T)
-  }
+  # Footer
   mtext("Elsevier Health Analytics",1,line=1,adj=1.0,cex=0.65,outer=T,font=3)
 }
 
 age_histogram_main <- function() {
   # Check if limit is set as a parameter
-  if(!is.null(params$limit)){
+  if(!is.null(params$limit) && params$limit > 0){
     # Patients with limit
     patients <- i2b2$crc$getPatientsWithLimit(model.patient_set, params$limit)
   } else {

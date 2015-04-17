d835e2d5ae61e7a3cb52d81071db1af9c5bb8345
Carl Ambroselli <git@carl-ambroselli.de>
2015-04-17 11:20:46 +0200

    set.alpha method added

--- GIRIXScripts/lib/style.r
+++ GIRIXScripts/lib/style.r
@@ -8,3 +8,12 @@ gray <- rgb(171, 171, 171, maxColorValue=255)
 darkGray <- rgb(83, 86, 90, maxColorValue=255)
 
 par(fg=darkGray, col=baseColor, col.axis=darkGray, col.lab=darkGray, col.main=darkGray)
+
+## Add an alpha value to a colour
+set.alpha <- function(col, alpha=1){
+  apply(
+    sapply(col, col2rgb)/255, 
+    2, 
+    function(x){ rgb(x[1], x[2], x[3], alpha=alpha) }
+   )  
+}

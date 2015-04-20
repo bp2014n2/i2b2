d82c9b70d2591a28c80257bb4df6f333eee4a900
Carl Ambroselli <git@carl-ambroselli.de>
2015-04-20 11:10:21 +0200

    Refactored frequence_of_visits_chart

--- GIRIXScripts/ReportGenerator/diagrams/frequence_of_visits_chart/diagram.r
+++ GIRIXScripts/ReportGenerator/diagrams/frequence_of_visits_chart/diagram.r
@@ -1,59 +1,79 @@
 # ---- code ----
-setwd("../../../")
+source("../../lib/style.r")
 
-frequence_of_visits <- function(patientsWithObservation, patientsWithoutObservation) {
+frequence_of_visits <- function(patients) {
  
-  par(mai=c(1,1,0.5,0.5),omi=c(0.65,0.25,0.75,0.75),family="Lato Light",las=1)
+  par(
+    mai=c(1,1,0.5,0.5),         # Outer border (bottom, top, left, right)
+    omi=c(0.65,0.25,0.75,0.75), # Inner border
+    family="Lato",              # Font 
+    las=1,                      # Style of axle labels (horizontal) 
+    col=darkGray
+  )
+
+  # Coverting visit date to timestamp
+  patients$visitsWithObservation$visitDate <- strptime(     
+    unlist(patients$visitsWithObservation[1], use.names=F),   # vector of all visits
+    "%Y-%m-%d %H:%M:%S"                                       # format how to parse
+  )
+
+  # Absolute count
+  patients$visitsWithObservation$abs_counts <- as.numeric(unlist(c(patients$visitsWithObservation[2])))
+
+  # Calculate relative count for with observation (number of visits) / (number of patients)
+  patients.withObservation.relative_visits_counts <- unlist(
+    lapply(
+      patients$visitsWithObservation$abs_counts,
+      FUN = function(r) {
+        r / patients$withObservation
+      }
+    )
+  )
+
+  visits_per_quarter <- quarters(patients$visitsWithObservation$visitDate)
+  years <- strftime(patients$visitsWithObservation$visitDate, "%Y")
+  dd <- data.frame(visits_per_quarter, years, patients.withObservation.relative_visits_counts)
+
+  p.with.agg <- aggregate(patients.withObservation.relative_visits_counts ~ visits_per_quarter + years, dd, FUN = sum)
+  p.with.agg$date <- as.POSIXct(paste(p.with.agg$years, as.numeric(substr(p.with.agg$visits_per_quarter,2,2))*3, "01", sep = "-"))
+
+  # Coverting visit date to timestamp
+  patients$visitsWithoutObservation$visitDate <- strptime(
+    unlist(patients$visitsWithoutObservation[1], use.names=F),  # vector of all visits
+    "%Y-%m-%d %H:%M:%S"                                         # format how to parse
+  )
+
+  # Absolute count
+  patients$visitsWithoutObservation$abs_counts <- as.numeric(unlist(c(patients$visitsWithoutObservation[2])))
+
+  # Calculate relative count for without observation (number of visits) / (number of patients)
+  patients.withoutObservation.relative_visits_counts <- unlist(
+    lapply(
+      patients$visitsWithoutObservation$abs_counts, 
+      FUN = function(r) {
+        r / patients$withoutObservation
+      }
+    )
+  )
+
+  visits_per_quarter <- quarters(patients$visitsWithoutObservation$visitDate)
+  years <- strftime(patients$visitsWithoutObservation$visitDate, "%Y")
+  dd <- data.frame(visits_per_quarter, years, patients.withoutObservation.relative_visits_counts)
+
+  p.without.agg <- aggregate(patients.withoutObservation.relative_visits_counts ~ visits_per_quarter + years, dd, FUN = sum)
+  p.without.agg$date <- as.POSIXct(paste(p.without.agg$years, as.numeric(substr(p.without.agg$visits_per_quarter,2,2))*3, "01", sep = "-"))
   
-  # Daten einlesen und Grafik vorbereiten
-  farbe1_150<-rgb(68,90,111,150,maxColorValue=255) 
-  farbe1_50<-rgb(68,90,111,50,maxColorValue=255)   
-  farbe2_150<-rgb(255,97,0,150,maxColorValue=255)  
-  farbe2_50<-rgb(255,97,0,50,maxColorValue=255)    
-  
-  # Grafik definieren und weitere Elemente
-
-  if(is.null(params$icd)) {
-    p.with.count <- i2b2$crc$getPatientsCountWithObservation()
-    p.without.count <- i2b2$crc$getPatientsCountWithoutObservation()
-  } else {
-    p.with.count <- i2b2$crc$getPatientsCountWithObservation(concept=params$icd)
-    p.without.count <- i2b2$crc$getPatientsCountWithoutObservation(concept=params$icd)
-  }
-  print(p.with.count)
-  print(p.without.count)
-
-  p.with.date <- strptime(unlist(patientsWithObservation[1], use.names=F), "%Y-%m-%d %H:%M:%S")
-  p.with.data <- unlist(lapply(as.numeric(unlist(c(patientsWithObservation[2]))), FUN = function(r) {r/p.with.count}))
-  print(patientsWithObservation)
-  mo <- quarters(p.with.date)
-  yr <- strftime(p.with.date, "%Y")
-  dd <- data.frame(mo, yr, p.with.data)
-
-  p.with.agg <- aggregate(p.with.data ~ mo + yr, dd, FUN = sum)
-  p.with.agg$date <- as.POSIXct(paste(p.with.agg$yr, as.numeric(substr(p.with.agg$mo,2,2))*3, "01", sep = "-"))
-
-  p.without.date <- strptime(unlist(patientsWithoutObservation[1], use.names=F), "%Y-%m-%d %H:%M:%S")
-  p.without.data <- unlist(lapply(as.numeric(unlist(c(patientsWithoutObservation[2]))), FUN = function(r) {r/p.without.count}))
-
-  mo <- quarters(p.without.date)
-  yr <- strftime(p.without.date, "%Y")
-  dd <- data.frame(mo, yr, p.without.data)
-
-  p.without.agg <- aggregate(p.without.data ~ mo + yr, dd, FUN = sum)
-  p.without.agg$date <- as.POSIXct(paste(p.without.agg$yr, as.numeric(substr(p.without.agg$mo,2,2))*3, "01", sep = "-"))
-  
-  plot(p.with.agg$date,p.with.agg$p.with.data,type="n",xlab="",ylab="Relative Anzahl")
+  plot(p.with.agg$date,p.with.agg$patients.withObservation.relative_visits_counts,type="n",xlab="",ylab="relative count")
 
   # axis(2,col=par("bg"),col.ticks="grey81", lwd.ticks=0.5,tck=-0.025)
-  lines(p.with.agg$date,p.with.agg$p.with.data,type="l",col=farbe1_150,lwd=3,xpd=T)
-  lines(p.without.agg$date,p.without.agg$p.without.data,type="l",col=farbe2_150,lwd=3)
-  text(head(p.with.agg$date, 1),max(p.without.agg$p.without.data, p.with.agg$p.with.data)*0.9,"Mit Knorpelkrankheiten",adj=0,cex=1,col=farbe1_150)
-  text(head(p.with.agg$date, 1),max(p.without.agg$p.without.data, p.with.agg$p.with.data)*0.85,"Ohne Knorpelkrankheiten",adj=0,cex=1,col=farbe2_150)
+  lines(p.with.agg$date,p.with.agg$patients.withObservation.relative_visits_counts,type="l",col=baseColor,lwd=3,xpd=T)
+  lines(p.without.agg$date,p.without.agg$patients.withoutObservation.relative_visits_counts,type="l",col=accentColor[1],lwd=3)
+  text(head(p.with.agg$date, 1),max(p.without.agg$patients.withoutObservation.relative_visits_counts, p.with.agg$patients.withObservation.relative_visits_counts)*0.9,"Mit Knorpelkrankheiten",adj=0,cex=1,col=baseColor)
+  text(head(p.with.agg$date, 1),max(p.without.agg$patients.withoutObservation.relative_visits_counts, p.with.agg$patients.withObservation.relative_visits_counts)*0.85,"Ohne Knorpelkrankheiten",adj=0,cex=1,col=accentColor[1])
   beginn<-c(head(p.with.agg$date, 1)); ende<-c(tail(p.with.agg$date, 1))
-  farbe<-c(farbe1_50)
+  farbe<-c(set.alpha(baseColor, 0.5))
   for(i in 1:length(beginn)) {
-    xx<-c(p.with.agg$date,rev(p.with.agg$date)); yy<-c(p.with.agg$p.with.data,rev(p.without.agg$p.without.data))
+    xx<-c(p.with.agg$date,rev(p.with.agg$date)); yy<-c(p.with.agg$patients.withObservation.relative_visits_counts,rev(p.without.agg$patients.withoutObservation.relative_visits_counts))
     polygon(xx,yy,col=farbe[i],border=F)
   }
   
@@ -68,16 +88,19 @@ frequence_of_visits <- function(patientsWithObservation, patientsWithoutObservat
 }  
 
 frequence_of_visits_main <- function() {
-  if(is.null(params$icd)) {
-    patientsWithObservation <- i2b2$crc$getVisitCountForPatientsWithObservation()
-    patientsWithoutObservation <- i2b2$crc$getVisitCountForPatientsWithoutObservation()
+  patients <- matrix()
+  if(is.null(params$icd)  || nchar(params$icd) == 0) {
+    patients$visitsWithObservation <- i2b2$crc$getVisitCountForPatientsWithObservation(model.patient_set)
+    patients$visitsWithoutObservation <- i2b2$crc$getVisitCountForPatientsWithoutObservation(model.patient_set)
+    patients$withObservation <- i2b2$crc$getPatientsCountWithObservation(model.patient_set)
+    patients$withoutObservation <- i2b2$crc$getPatientsCountWithoutObservation(model.patient_set)
   } else {
-    print(c(params$icd))
-    patientsWithObservation <- i2b2$crc$getVisitCountForPatientsWithObservation(concepts=c(params$icd))
-    patientsWithoutObservation <- i2b2$crc$getVisitCountForPatientsWithoutObservation(concepts=c(params$icd))
+    patients$visitsWithObservation <- i2b2$crc$getVisitCountForPatientsWithObservation(patient_set=model.patient_set, concepts=c(params$icd))
+    patients$visitsWithoutObservation <- i2b2$crc$getVisitCountForPatientsWithoutObservation(patient_set=model.patient_set, concepts=c(params$icd))
+    patients$withObservation <- i2b2$crc$getPatientsCountWithObservation(patient_set=model.patient_set, concept=params$icd)
+    patients$withoutObservation <- i2b2$crc$getPatientsCountWithoutObservation(patient_set=model.patient_set, concept=params$icd)
   }
-  print(patientsWithObservation)
-  frequence_of_visits(patientsWithObservation, patientsWithoutObservation)
+  frequence_of_visits(patients)
 }
 
 frequence_of_visits_main()

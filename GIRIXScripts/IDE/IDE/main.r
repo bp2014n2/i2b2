0c1137efa54bac8b2d62f72bd5d343bb8f535d85
Carl Ambroselli <git@carl-ambroselli.de>
2015-04-30 11:29:00 +0200

    Added IDE scriptlet

---
+++ GIRIXScripts/IDE/IDE/main.r
@@ -0,0 +1,57 @@
+# Install package necessary
+list.of.packages <- c("knitr", "extrafont")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Load necessary libraries
+library(knitr); 
+library(jsonlite)
+library(extrafont)
+source("../lib/i2b2.r", chdir=TRUE)
+
+generateOutput <- function() {
+  params <<- fromJSON(girix.input["params"])
+  model.patient_set <<- ifelse(nchar(girix.input['Patient Set']) != 0, strtoi(girix.input['Patient Set']), -1)
+
+  #Don't print warnings
+  options(warn=-1)
+
+  # Generate unique information for temporary files
+  randomNumber <- floor(runif(1, 10000000, 99999999))
+  currentTimestamp <- print(as.numeric(Sys.time()))
+  tmpFolder <- paste('IDE/tmp/', randomNumber, currentTimestamp, "/", sep='')
+
+  # Create temporary folder
+  dir.create(tmpFolder, mode="0777", recursive=T)
+  dir.create(paste(tmpFolder, '/plots',  sep=''), mode="0777")
+
+  # Deactivates code output globally
+  knitr::opts_chunk$set(echo=FALSE, fig.path=paste0(tmpFolder, 'plots/'), cache=FALSE, dev='svg', results='hide')
+#  opts_knit$set(progress = FALSE, verbose = FALSE)
+  opts_chunk$set(fig.width=5, fig.height=5)
+
+  # Embed SVGs in HTML
+  hook_plot = knit_hooks$get('plot')
+  knit_hooks$set(plot = function(x, options) {
+   x = paste(x, collapse = '.')
+   if (!grepl('\\.svg', x)) return(hook_plot(x, options))
+   # read the content of the svg image and write it out without <?xml ... ?>
+   paste("<img src='data:image/svg+xml;utf8,", paste(readLines(x)[-1], collapse = '\n'), "'>", sep="")
+  })
+
+  # Generate File
+  fileName <- 'main.html'
+  if(girix.input["requestOutput"] == "all"){
+    knit(paste0('layout/main.Rhtml'), output=paste(tmpFolder, fileName, sep=""))
+  } else {
+    print(params)
+    knit(paste0('IDE/diagram.Rhtml'), output=paste(tmpFolder, fileName, sep=""))
+  }
+
+  output <- readChar(paste(tmpFolder, fileName, sep=""), file.info(paste(tmpFolder, fileName, sep=""))$size) 
+
+  #Cleanup
+  unlink(tmpFolder, recursive=T)
+
+  return(output)
+}
---
+++ GIRIXScripts/IDE/diagrams/map/diagram.r
@@ -0,0 +1,72 @@
+# ---- code ----
+# suppressMessages(library(maptools))
+# suppressMessages(library(rgdal))
+suppressMessages(library(sp))
+library(RColorBrewer)
+# gpclibPermit()
+
+map <- function(){
+  options(scipen=10) # don't convert into exponential format
+
+  par(
+    mfrow = c(1,2),
+    mar=c(0,0,0,0),
+    oma=c(1,1,1,0), 
+    mfcol=c(1,1),
+    family="Lato",
+    las=1
+  )
+  
+  # Generate map data and save to file [already saved to map_data.RData]
+  # map_data<-readShapeSpatial(
+  #  "/home/ubuntu/i2b2/GIRIXScripts/ReportGenerator/diagrams/map/post_pl.shp",
+  #  IDvar="PLZ99", 
+  #  proj4string=CRS("+proj=longlat")
+  # )
+  # 
+  # map_data<-spTransform(map_data,CRS=CRS("+proj=merc"))
+  # map_data$zip <- as.numeric(substr(map_data$PLZ99, 1, 2))
+  # 
+  # slot(map_data, "polygons") <- slot(unionSpatialPolygons(map_data, map_data$zip), "polygons")
+  # 
+  # postals <- unique(slot(map_data, "data")$zip)
+  # slot(map_data, "plotOrder") <- seq(1,length(postals))
+
+  # polygon_data <- data.frame(zip=as.numeric(sapply(slot(map_data, "polygons"), function(i) slot(i, "ID"))))
+  # slot(map_data, "data") <- polygon_data
+  # 
+  # save(map_data, file="map_data.RData")
+
+  load("../diagrams/map/map_data.RData")
+
+  color<-sample(1:7,length(map_data),replace=T)
+
+  patient_data <- i2b2$crc$getPatientsWithPlz(model.patient_set)
+  patient_data$zip <- substring(patient_data$statecityzip_path, nchar(patient_data$statecityzip_path)-2, nchar(patient_data$statecityzip_path)-1)
+
+  zip_data <- aggregate(x=patient_data$count, by=list(patient_data$zip), FUN=sum)
+  zip_data$zip <- as.numeric(zip_data[,1])
+
+ #  if(!is.null(params$plzFilter) && nchar(params$plzFilter) > 0) {
+ #    indexInSet <- which(slot(map_data, "data") == params$plzFilter)[1]
+ #    slot(map_data, "data") <- as.data.frame(slot(map_data, "data")[14,])
+ #    slot(map_data, "plotOrder") <- seq(1,1)
+ #  }
+
+  tmp_data <- merge(x=slot(map_data, "data"), y=zip_data, by="zip", all.x=T) 
+  tmp_data$x[is.na(tmp_data$x)] <- 0 # Replace NA with 0
+  slot(map_data, "data") <- tmp_data
+
+  tmp_data$zip <- as.character(tmp_data$zip)
+  sorted_data_alphanumerical <- tmp_data[order(tmp_data$zip),]$x
+
+  color_nr <- cut(sorted_data_alphanumerical, c(0,10,1000,2000,4000,10000,100000),dig.lab=10)
+  colors<-brewer.pal(6, "Oranges")
+
+  # Plot
+  plot(map_data,col=colors[color_nr],border = "black", lwd = 1)
+
+  # Legend
+  legend("bottomleft", levels(color_nr), cex=0.95,border=F,bty="n",fill=colors)
+}
+map()

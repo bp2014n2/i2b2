282a8c5e93de17911379595a507d77efe9e5203c
Carl Ambroselli <git@carl-ambroselli.de>
2015-05-04 10:43:32 +0200

    Pretty print of data.frames and matrixes

--- GIRIXScripts/IDE/templates/editor/diagram.r
+++ GIRIXScripts/IDE/templates/editor/diagram.r
@@ -1,5 +1,22 @@
 # ---- code ----
 source("../../lib/style.r")
+library(xtable)
+
+print_table <- function(name) {
+  print(xtable(name), type = "html")
+}
+
+
+print.data.frame <- print_table
+print.matrix <- print_table
+
+# print <- function(x, ...) {
+#   if(typeof(x) == "data.frame" || typeof(x) == "matrix" || typeof(x) == "list"){
+#     print_table(x)
+#   } else {
+#     base::print(x, ...)
+#   }
+# }
 
 if(!is.null(params)) {
   eval(parse(text=params$code))

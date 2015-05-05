caa6f0218522b9bc84ce224930fc4b85e17bebce
Carl Ambroselli <git@carl-ambroselli.de>
2015-05-05 09:30:35 +0200

    Updates IDE sample code to contain patients

--- GIRIXScripts/IDE/templates/editor/diagram.r
+++ GIRIXScripts/IDE/templates/editor/diagram.r
@@ -6,22 +6,10 @@ print_table <- function(name) {
   print(xtable(name), type = "html")
 }
 
-
 print.data.frame <- print_table
 print.matrix <- print_table
 
-sink("/dev/null") # On Windows use sink("NUL")
 patients <- i2b2$crc$getPatients(model.patient_set, silent=T)
-sink()
-
-
-# print <- function(x, ...) {
-#   if(typeof(x) == "data.frame" || typeof(x) == "matrix" || typeof(x) == "list"){
-#     print_table(x)
-#   } else {
-#     base::print(x, ...)
-#   }
-# }
 
 if(!is.null(params)) {
   eval(parse(text=params$code))

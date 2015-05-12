3ef873a7ff000d9b368e5b80bf1ebe431dad72fc
Philipp Schirmer <philipp@schirmer-online.de>
2015-05-12 11:05:22 +0200

    ...

--- GIRIXScripts/PSM/girix_input.r
+++ GIRIXScripts/PSM/girix_input.r
@@ -5,6 +5,6 @@ setwd('PSM')
 girix.input['Treatment group'] <- "27"   #in webclient: L10 Pemphiguskr, 7. april, 487 patients
 girix.input['Control group'] <- "29"    #in webclient: "G70-G73 Krankhe@10:47:54" , 7. april, 8067 patients
 girix.input['Treatment Quarter'] <- 'c("year"=2006,"quarter"=2)'
-girix.input['Feature level'] <- "2"
-girix.input['Feature Selection'] <- 'c("ICD"=TRUE,"ATC"=FALSE)'
+girix.input['Feature level'] <- "3"
+girix.input['Feature Selection'] <- 'c("ICD"=TRUE,"ATC"=TRUE)'
 girix.input['Exact matching'] <- 'c("Age"=TRUE,"Gender"=TRUE)'

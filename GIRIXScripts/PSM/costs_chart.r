629c874511ec009852842bb7a2c6120e7902a560
Carl Ambroselli <git@carl-ambroselli.de>
2015-05-13 12:53:10 +0200

    Fixes typos

--- GIRIXScripts/PSM/costs_chart.r
+++ GIRIXScripts/PSM/costs_chart.r
@@ -2,7 +2,7 @@ library(plotrix)
 
 costs_chart <- function(control, treated) {
 
-  par(pin=c(5,3), mai=c(0.5,1.1,0,0.5),omi=c(0.5,0.5,0.8,0.5),las=1, mfrow=c(1,2))
+  par(mai=c(0.2,0.2,0.2,0.2),omi=c(0.2,0.2,0.2,0.2),las=1)
   cost_chart(control)
   cost_chart(treated)
 
@@ -18,7 +18,7 @@ cost_chart <- function(group) {
   group$datum <- NULL
   group$summe_aller_kosten <- NULL
 
-  stackpoly(group, main="",xaxlab=rep("", nrow(control)),boder="white", stack=TRUE, axis2=F, col=colors)
+  stackpoly(group, main="",xaxlab=rep("", nrow(group)),border="white", stack=TRUE, axis2=F, col=colors)
 
   lines(total, lwd=4, col="lightgoldenrod4")
   name <- names(control)

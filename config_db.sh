a09c3cfb6325b1b97824b600dec1508263def99a
Philipp Schirmer <philipp.schirmer@student.hpi.uni-potsdam.de>
2015-05-24 21:01:22 +0200

    fixes

--- config_db.sh
+++ config_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-. env.properties
+. ./env.properties
 
 sed "s|54\.93\.194\.65:5432|`echo $DB_LOC`|g" */etc/jboss/*-ds.xml -i
 sed "s|54\.93\.194\.65:5432|`echo $DB_LOC`|g" */etc/spring/*LoaderApplicationContext.xml -i
+
+git update-index --assume-unchanged */etc/jboss/*-ds.xml;
--- install/com.sh
+++ install/com.sh
@@ -5,8 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $DIR/../env.properties
 
 apt-get -y install openjdk-7-jdk unzip curl
-cd ~
-curl -s -o ~/jboss.zip http://54.93.194.56/jboss.zip
+curl -s -o jboss.zip http://54.93.194.56/jboss.zip
 mkdir $JBOSS_HOME
 unzip -d $JBOSS_HOME jboss.zip
-rm -rf ~/jboss.zip
+rm -rf jboss.zip

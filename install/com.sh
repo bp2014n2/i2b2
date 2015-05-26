69af0bbf395f231f3ea1e3922aa54becf2ac6951
Philipp Schirmer <philipp.schirmer@student.hpi.uni-potsdam.de>
2015-05-26 10:56:25 +0200

    jboss service

--- install/com.sh
+++ install/com.sh
@@ -9,3 +9,7 @@ curl -s -o jboss.zip http://54.93.194.56/jboss.zip
 mkdir -p $JBOSS_HOME
 unzip -d $JBOSS_HOME jboss.zip
 rm -rf jboss.zip
+cp ./jboss.service /etc/init.d/jboss
+chmod 755 /etc/init.d/jboss
+update-rc.d jboss defaults
+service jboss start

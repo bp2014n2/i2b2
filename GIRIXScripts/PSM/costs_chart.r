20618f9caa2c3c212ce50055a0515c51ee8cba15
Philipp Schirmer <philipp@schirmer-online.de>
2015-06-01 10:50:06 +0200

    fixed costs chart

--- GIRIXScripts/PSM/costs_chart.r
+++ GIRIXScripts/PSM/costs_chart.r
@@ -9,7 +9,7 @@ costs_chart <- function(control, treated) {
   binded$datum <- NULL
   binded$summe_aller_kosten <- NULL
   sorting <- order(sapply(binded, var))
-  par(mai=c(0.2,0.2,0.2,0.2),omi=c(0.2,0.2,0.2,0.2),las=1, col="black")
+  #par(mai=c(1.2,1.2,1.2,1.2),omi=c(0.2,0.2,0.2,0.2),las=1, col="black")
   ymax <- max(treated$summe_aller_kosten, control$summe_aller_kosten)
   ymin <- min(treated$summe_aller_kosten, control$summe_aller_kosten, 0)
   
@@ -19,20 +19,25 @@ costs_chart <- function(control, treated) {
 }
 
 cost_chart <- function(group, name, ylim, sorting) {
-  par(mai=c(0.2,0.2,0.2,0.2),omi=c(0.2,0.2,0.2,0.2),las=1, col="black")
+  op <- par(col="black")
 
   colors <- rev(tetradicColors(baseColor, 8))
+  p.col <<- colors
 
-  years <- group$datum
+  years <- format(group$datum, "%Y")
   total <- group$summe_aller_kosten
+  p.years <<- years
+  p.ylim <<- ylim
 
   group$datum <- NULL
   group$summe_aller_kosten <- NULL
 
   group <- group[sorting]
+  p.group <<- group
 
-  stackpoly(group, main="",xaxlab=rep("", nrow(group)),border="white", stack=TRUE, axis2=F, col=colors, ylim=ylim)
+  stackpoly(group, main="",xaxlab=years,border="white", stack=TRUE, axis2=F, col=colors, ylim=ylim)
 
   legend("topright", name)
   legend("topleft", rev(names(group)), cex=0.95,border=F,bty="n",fill=rev(colors))
+  par(op)
 }

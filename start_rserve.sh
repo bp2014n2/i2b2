e398bc5db1a6d388c3f700058c00386e16c6258c
Philipp Schirmer <philipp@schirmer-online.de>
2015-06-01 11:05:27 +0200

    rserve home property

--- start_rserve.sh
+++ start_rserve.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/lib/R/bin/R CMD /usr/local/lib/R/site-library/Rserve/libs/Rserve --no-save --RS-port 6311
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+. $DIR/env.properties
+
+/usr/lib/R/bin/R CMD $RSERVE_HOME/libs/Rserve --no-save --RS-port 6311

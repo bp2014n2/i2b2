08faf80b091879171da1e4223aa936cb50f9185e
Philipp Schirmer <philipp@schirmer-online.de>
2015-07-21 11:46:09 +0200

    Configuration resets files

--- config_db.sh
+++ config_db.sh
@@ -2,7 +2,9 @@
 
 . ./env.properties
 
+git checkout */etc/jboss/*-ds.xml */etc/spring/*LoaderApplicationContext.xml
 sed "s|54\.93\.194\.65:5432|`echo $DB_LOC`|g" */etc/jboss/*-ds.xml -i
 sed "s|54\.93\.194\.65:5432|`echo $DB_LOC`|g" */etc/spring/*LoaderApplicationContext.xml -i
 
 git update-index --assume-unchanged */etc/jboss/*-ds.xml;
+git update-index --assume-unchanged */etc/spring/*LoaderApplicationContext.xml;
--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . ./env.properties
+git checkout */build.properties */etc/spring/*_application_directory.properties
 sh config_db.sh $db_loc
 sed "s|\${env\.I2B2_HOME}|`echo $I2B2_HOME`|g" */build.properties -i
 sed "s|\${env\.WWW_LOC}|`echo $WWW_LOC`|g" */build.properties -i
@@ -8,6 +9,5 @@ sed "s|\${env\.GIRIX_ASSETS}|`echo $GIRIX_ASSETS`|g" */build.properties -i
 sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */build.properties -i
 sed "s|\${env\.JBOSS_HOME}|`echo $JBOSS_HOME`|g" */etc/spring/*_application_directory.properties -i
 
-git update-index --assume-unchanged */etc/spring/*LoaderApplicationContext.xml;
-git update-index --assume-unchanged */build.properties;
 git update-index --assume-unchanged */etc/spring/*_application_directory.properties;
+git update-index --assume-unchanged */build.properties;
